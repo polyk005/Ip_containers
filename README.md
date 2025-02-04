dedeb020b983a93315a1365b185225a95fd3d942
Midiy <polyk6859@gmail.com>
2025-02-04 18:51:57 +0300

    finish progect

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ip_containers
+
+//ссылка на вход - http://localhost:8090/api/containers
--- pinger/pkg/api/api.go
+++ pinger/pkg/api/api.go
@@ -19,7 +19,7 @@ func SendData(ip, pingTime, lastSuccessfulPing string) error {
 		return fmt.Errorf("failed to marshal payload: %v", err)
 	}
 
-	resp, err := http.Post("http://backend:8080/api/containers", "application/json", bytes.NewBuffer(jsonData))
+	resp, err := http.Post("http://back:8080/api/containers", "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
 		return fmt.Errorf("failed to send request: %v", err)
 	}
